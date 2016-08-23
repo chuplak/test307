dfa871206d8219a4f2fcebf2aa6880a661e9c4bd
Roman Chuplak <Roman Chuplak>
2016-08-23 21:46:11 +0300

    2016

--- pangram.py
+++ pangram.py
@@ -1,8 +1,9 @@
+# Copyright 2016
 from string import ascii_lowercase as az
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
 def check(text):
 	return set(text.lower()).issuperset(set(az))
 
-print(check("abc"))
 
+print(check("abc"))
