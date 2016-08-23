22f3f65b3419eb2ad1c3be9e7ca8846d5331d3b0
Roman Chuplak <Roman Chuplak>
2016-08-23 20:51:56 +0300

    ascii_lowercase

--- pangram.py
+++ pangram.py
@@ -2,7 +2,7 @@ from string import ascii_lowercase as az
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
 def check(text):
-	return set(text.lower()).issuperset(set(alphabet))
+	return set(text.lower()).issuperset(set(az))
 
 print(check("abc"))
 
