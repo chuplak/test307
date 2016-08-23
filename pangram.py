b700a5fe2c2e73fe0b5970f189717e588eea569e
Roman Chuplak <Roman Chuplak>
2016-08-23 21:29:46 +0300

    Added ful alphabet

--- pangram.py
+++ pangram.py
@@ -3,6 +3,7 @@ alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
 def check(text):
 	return set(text.lower()).issuperset(set(az))
-
-print(check("abc"))
-
+	
+	
+	print(check("abc") == False)
+	print(check("abcdefghijklmnopqrstuvwxyz") == True)
